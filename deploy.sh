ebcb4889d508c88cc1a32d3ff54680633a84d3a6
jc <jjcfrm@163.com>
2021-10-09 14:31:45 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add .
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f https://github.com/NameJJC/namejjc.github.io.git master
+git push -f https://github.com/NameJJC/namejjc.github.io.git origin master
 #git push -f origin master
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
